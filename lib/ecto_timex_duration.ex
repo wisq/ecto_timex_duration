ad5c9ba284e0e88926e48affcf460f46f89686d6
Adrian Irving-Beer <wisq@wisq.net>
2022-03-30 21:33:06 -0400

    `mix new ecto_timex_duration`

---
+++ lib/ecto_timex_duration.ex
@@ -0,0 +1,18 @@
+defmodule EctoTimexDuration do
+  @moduledoc """
+  Documentation for `EctoTimexDuration`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoTimexDuration.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_timex_duration_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoTimexDurationTest do
+  use ExUnit.Case
+  doctest EctoTimexDuration
+
+  test "greets the world" do
+    assert EctoTimexDuration.hello() == :world
+  end
+end
